c515df26318684afe5f2b58792f58ae061c217b0
jyupiter <argonteal@gmail.com>
2021-11-30 17:18:23 +0800

    Tidied up scripts
    Moved some logic from GameController.cs to CustomerManager.cs

--- Assets/Scripts/Game/Observer/Level.cs
+++ Assets/Scripts/Game/Observer/Level.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
 
-
     void Start()
     {
-        CustomerManager.PopulateCustomers();
-        foreach(Customer c in CustomerManager.customers)
-        {
-            Debug.Log(c.GetCustomerName() + ", " + c.GetOrder().GetBread());
-        }
     }
 
     void Update()
