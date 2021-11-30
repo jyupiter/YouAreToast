7f9224aa13253d7cf5d921d99b2126d3bf12f553
jyupiter <argonteal@gmail.com>
2021-11-30 16:11:23 +0800

    Refactored customer/order scripts
    Also put something in the scene background

---
+++ Assets/Scripts/Game/FileIO.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileIO : MonoBehaviour
+{
+    private static List<string> nameList, spriteList;
+    private List<string> ReadTextDataToList(string textFileInput)
+    {
+        string[] data =
+        File.ReadAllLines
+        (
+            Path.Combine
+            (
+                Application.streamingAssetsPath,
+                textFileInput
+            )
+        );
+        return new List<string>(data);
+    }
+
+    private void PopulateLists()
+    {
+        nameList = ReadTextDataToList("names.txt");
+        spriteList = ReadTextDataToList("sprites.txt");
+    }
+
+    void Start()
+    {
+        PopulateLists();
+    }
+
+    public static List<string> GetNameList()
+    {
+        return nameList;
+    }
+
+    public static List<string> GetSpriteList()
+    {
+        return spriteList;
+    }
+}
---
+++ Assets/Scripts/Game/Observer/Level.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        CustomerManager.PopulateCustomers();
+        foreach(Customer c in CustomerManager.customers)
+        {
+            Debug.Log(c.GetCustomerName() + ", " + c.GetOrder().GetBread());
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
