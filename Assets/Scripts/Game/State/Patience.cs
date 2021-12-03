d29df1619c4a397a8bf0f3570af2bfe132140f04
Vausta2023 <72809508+Vausta2023@users.noreply.github.com>
2021-12-03 12:14:20 +0800

    aaaaaaaaaaa

--- Assets/Scripts/Game/State/Patience.cs
+++ Assets/Scripts/Game/State/Patience.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Patience : MonoBehaviour, ICustomerStates
+public class Patience : PatienceStates
 {
     private int minPatience = 50;
     private int maxPatience = 90;
@@ -11,38 +11,8 @@ public class Patience : MonoBehaviour, ICustomerStates
     public static float randomPatience;
     public static float currPatience;
 
-    public SpriteRenderer expression;
-
-    [HideInInspector] public CustomerManager cm;
-
-    public void CustomerHappy()
-    {
-        expression.sprite = FileIO.GetCustomerSpriteLists()[1][0];
-        //TODO: When toast is given to customer: Compare product with expectation.
-            //If correct order is given, give high tips
-            //If wrong order, give small tips
-    }
-    public void CustomerImpatient()
-    {
-        expression.sprite = FileIO.GetCustomerSpriteLists()[1][1];
-        //TODO: When toast is given to customer: Compare product with expectation.
-        //If correct order is given, give small tips
-        //If wrong order, give no tips
-    }
-    public void CustomerAngry()
-    {
-        expression.sprite = FileIO.GetCustomerSpriteLists()[1][2];
-        //TODO: When toast is given to customer: Compare product with expectation.
-        //If correct order is given, give no tips
-        //If wrong order, dont even pay for the toast.
-    }
-    public void CustomerLeave()
-    {
-        cm.HideThisCustomer();
-    }
     public void Awake()
     {
-        cm = GameObject.Find("Scripts").GetComponent<CustomerManager>();
         randomPatience = GameController.r.Next(minPatience, maxPatience);
         currPatience = randomPatience;
     }
---
+++ Assets/Scripts/Game/State/PatienceStates.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatienceStates : MonoBehaviour, ICustomerStates
+{
+    public SpriteRenderer expression;
+    [HideInInspector] public CustomerManager cm;
+    private void Awake()
+    {
+        cm = GameObject.Find("Scripts").GetComponent<CustomerManager>();
+    }
+    public void CustomerHappy()
+    {
+        expression.sprite = FileIO.GetCustomerSpriteLists()[1][0];
+        //TODO: When toast is given to customer: Compare product with expectation.
+        //If correct order is given, give high tips
+        //If wrong order, give small tips
+    }
+    public void CustomerImpatient()
+    {
+        expression.sprite = FileIO.GetCustomerSpriteLists()[1][1];
+        //TODO: When toast is given to customer: Compare product with expectation.
+        //If correct order is given, give small tips
+        //If wrong order, give no tips
+    }
+    public void CustomerAngry()
+    {
+        expression.sprite = FileIO.GetCustomerSpriteLists()[1][2];
+        //TODO: When toast is given to customer: Compare product with expectation.
+        //If correct order is given, give no tips
+        //If wrong order, dont even pay for the toast.
+    }
+    public void CustomerLeave()
+    {
+        cm.HideThisCustomer();
+    }
+}
